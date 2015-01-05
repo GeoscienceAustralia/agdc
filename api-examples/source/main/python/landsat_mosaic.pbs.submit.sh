78a71dfa151936c8fd266d13e8b82b67e764f903
Simon Oldfield <Simon.Oldfield@ga.gov.au>
2015-01-06 09:37:45 +1100

    Enable submission of the full set (almost) of mosaic jobs

--- api-examples/source/main/python/landsat_mosaic.pbs.submit.sh
+++ api-examples/source/main/python/landsat_mosaic.pbs.submit.sh
@@ -29,16 +29,6 @@
 
 PBS_SCRIPT="$HOME/source/agdc-api/api-examples/source/main/python/landsat_mosaic.pbs.sh"
 
-#OUTPUT_DIR="/g/data/u46/sjo/output/landsat_mosaic"
-
-#qsub -v outputdir="${OUTPUT_DIR}",xmin=110,xmax=155,ymin=-45,ymax=-10,yearmin=2014,yearmax=2014 "${PBS_SCRIPT}"
-
-#qsub -v outputdir="${OUTPUT_DIR}",xmin=110,xmax=119,ymin=-45,ymax=-10,yearmin=1987,yearmax=2014 "${PBS_SCRIPT}"
-#qsub -v outputdir="${OUTPUT_DIR}",xmin=120,xmax=129,ymin=-45,ymax=-10,yearmin=1987,yearmax=2014 "${PBS_SCRIPT}"
-#qsub -v outputdir="${OUTPUT_DIR}",xmin=130,xmax=139,ymin=-45,ymax=-10,yearmin=1987,yearmax=2014 "${PBS_SCRIPT}"
-#qsub -v outputdir="${OUTPUT_DIR}",xmin=140,xmax=149,ymin=-45,ymax=-10,yearmin=1987,yearmax=2014 "${PBS_SCRIPT}"
-#qsub -v outputdir="${OUTPUT_DIR}",xmin=150,xmax=155,ymin=-45,ymax=-10,yearmin=1987,yearmax=2014 "${PBS_SCRIPT}"
-
 # LS5/7 mosaic with PQA applied
 qsub -v outputdir="/g/data/u46/sjo/output/landsat_mosaic_ls57_pqa",xmin=120,xmax=125,ymin=-25,ymax=-20,acqmin=2005-01,acqmax=2005-01 "${PBS_SCRIPT}"
 
@@ -46,22 +36,22 @@ qsub -v outputdir="/g/data/u46/sjo/output/landsat_mosaic_ls57_pqa",xmin=120,xmax
 qsub -v outputdir="/g/data/u46/sjo/output/landsat_mosaic_ls57",xmin=120,xmax=125,ymin=-25,ymax=-20,acqmin=2005-01,acqmax=2005-01,pqfilter=false "${PBS_SCRIPT}"
 
 # LS5 mosaic with PQA applied
-#qsub -v outputdir="/g/data/u46/sjo/output/landsat_mosaic_ls5_pqa",xmin=120,xmax=125,ymin=-25,ymax=-20,acqmin=2005-01,acqmax=2005-01,satellites=LS5 "${PBS_SCRIPT}"
+qsub -v outputdir="/g/data/u46/sjo/output/landsat_mosaic_ls5_pqa",xmin=120,xmax=125,ymin=-25,ymax=-20,acqmin=2005-01,acqmax=2005-01,satellites=LS5 "${PBS_SCRIPT}"
 
 # LS5 mosaic without PQA applied
-#qsub -v outputdir="/g/data/u46/sjo/output/landsat_mosaic_ls5",xmin=120,xmax=125,ymin=-25,ymax=-20,acqmin=2005-01,acqmax=2005-01,satellites=LS5,pqfilter=false "${PBS_SCRIPT}"
+qsub -v outputdir="/g/data/u46/sjo/output/landsat_mosaic_ls5",xmin=120,xmax=125,ymin=-25,ymax=-20,acqmin=2005-01,acqmax=2005-01,satellites=LS5,pqfilter=false "${PBS_SCRIPT}"
 
 # LS7 mosaic with PQA applied
-#qsub -v outputdir="/g/data/u46/sjo/output/landsat_mosaic_ls7_pqa",xmin=120,xmax=125,ymin=-25,ymax=-20,acqmin=2005-01,acqmax=2005-01,satellites=LS7 "${PBS_SCRIPT}"
+qsub -v outputdir="/g/data/u46/sjo/output/landsat_mosaic_ls7_pqa",xmin=120,xmax=125,ymin=-25,ymax=-20,acqmin=2005-01,acqmax=2005-01,satellites=LS7 "${PBS_SCRIPT}"
 
 # LS7 mosaic without PQA applied
-#qsub -v outputdir="/g/data/u46/sjo/output/landsat_mosaic_ls7",xmin=120,xmax=125,ymin=-25,ymax=-20,acqmin=2005-01,acqmax=2005-01,satellites=LS7,pqfilter=false "${PBS_SCRIPT}"
+qsub -v outputdir="/g/data/u46/sjo/output/landsat_mosaic_ls7",xmin=120,xmax=125,ymin=-25,ymax=-20,acqmin=2005-01,acqmax=2005-01,satellites=LS7,pqfilter=false "${PBS_SCRIPT}"
 
 # LS8 mosaic with PQA applied
-#qsub -v outputdir="/g/data/u46/sjo/output/landsat_mosaic_ls8_pqa",xmin=120,xmax=125,ymin=-25,ymax=-20,acqmin=2014-01,acqmax=2014-01,satellites=LS8 "${PBS_SCRIPT}"
+qsub -v outputdir="/g/data/u46/sjo/output/landsat_mosaic_ls8_pqa",xmin=120,xmax=125,ymin=-25,ymax=-20,acqmin=2014-01,acqmax=2014-01,satellites=LS8 "${PBS_SCRIPT}"
 
 # LS8 mosaic without PQA applied
-#qsub -v outputdir="/g/data/u46/sjo/output/landsat_mosaic_ls8",xmin=120,xmax=125,ymin=-25,ymax=-20,acqmin=2014-01,acqmax=2014-01,satellites=LS8,pqfilter=false "${PBS_SCRIPT}"
+qsub -v outputdir="/g/data/u46/sjo/output/landsat_mosaic_ls8",xmin=120,xmax=125,ymin=-25,ymax=-20,acqmin=2014-01,acqmax=2014-01,satellites=LS8,pqfilter=false "${PBS_SCRIPT}"
 
 # TODO
 
