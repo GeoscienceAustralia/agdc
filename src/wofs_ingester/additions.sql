ceff08c85a74f8c0c34e1b613ef034f91cfd5099
Jeremy Hooke <jez@stulk.com>
2015-01-12 09:44:00 +1100

    Use existing sequence for new DB type ids.

--- src/wofs_ingester/additions.sql
+++ src/wofs_ingester/additions.sql
@@ -1,20 +1,21 @@
 -- Water types
 
-insert into band_type (band_type_id, band_type_name)
-values (50, 'WATER');
+insert into band_type (band_type_name) values ('WATER');
 
-insert into public.band (band_id, sensor_id, band_name, band_type_id,
+insert into public.band (sensor_id, band_name, band_type_id,
                          file_number,
                          resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
-values (34, null, 'WATER', 50,
+values (null, 'WATER',
+        (select band_type_id from band_type where band_type_name = 'WATER'),
         10, -- ??
         null, null, null, '.*_WATER_.*', null, 'WATER', 1);
 
-insert into processing_level (level_id, level_name, nodata_value, resampling_method, level_description)
-values (6, 'WATER', 0, 'near', 'Water');
+insert into processing_level (level_name, nodata_value, resampling_method, level_description)
+values ('WATER', 0, 'near', 'Water');
 
 insert into band_source (tile_type_id, band_id, level_id, tile_layer)
 values (1,
-        34, 6,
+        (select band_type_id from band_type where band_type_name = 'WATER'),
+        (select level_id from processing_level where level_name='WATER'),
         1 -- ??
 );
