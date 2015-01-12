d7368ad18413bef072db57684274c6bb9fd87d46
Jeremy Hooke <jez@stulk.com>
2015-01-12 19:30:16 +1100

    Correct band->tile db mapping

--- src/wofs_ingester/additions.sql
+++ src/wofs_ingester/additions.sql
@@ -7,7 +7,7 @@ insert into public.band (sensor_id, band_name, band_type_id,
                          resolution, min_wavelength, max_wavelength, file_pattern, satellite_id, band_tag, band_number)
 values (null, 'WATER',
         (select band_type_id from band_type where band_type_name = 'WATER'),
-        10, -- ??
+        1, -- ??
         null, null, null, '.*_WATER_.*', null, 'WATER', 1);
 
 insert into processing_level (level_name, nodata_value, resampling_method, level_description)
@@ -15,7 +15,7 @@ values ('WATER', 0, 'near', 'Water');
 
 insert into band_source (tile_type_id, band_id, level_id, tile_layer)
 values (1,
-        (select band_type_id from band_type where band_type_name = 'WATER'),
+        (select band_id from band where band_name = 'WATER'),
         (select level_id from processing_level where level_name='WATER'),
         1 -- ??
 );
