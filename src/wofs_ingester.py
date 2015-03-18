2fe266481e993030ba751851946b1a3c1937cef4
Jeremy Hooke <jez@stulk.com>
2015-03-18 13:25:29 +1100

    Replace WOfS package with a single WOfS file.

--- src/wofs_ingester.py
+++ src/wofs_ingester.py
@@ -27,7 +27,18 @@
 # ===============================================================================
 
 """
-Ingestion of water tiles.
+Ingest WOfS tiles into the Data Cube.
+
+Example usage:
+
+Ingest all datasets within a folder (searched recursively):
+
+    python -m agdc.wofs_ingester -C datacube.conf --source /path/to/datasets
+
+Or a specific dataset:
+
+    python -m agdc.wofs_ingester -C datacube.conf --source LS7_ETM_WATER_140_-027_2013-08-18T00-26-16.880864.tif
+
 """
 
 import logging
