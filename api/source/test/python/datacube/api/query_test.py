c8fe98d0fdafbfaf009a1a1ceda03a0d5a0a4314
Simon Oldfield <simon@oldfield.id.au>
2015-05-25 11:23:37 +1000

    - #55 Enhance the query API to support only including datasets based on the month of acquisition - #56 Enhance the query API to support excluding datasets for a particular satellite based on their acquisition date - Add beginnings of pytest based unit tests for query API

--- api/source/test/python/datacube/api/query_test.py
+++ api/source/test/python/datacube/api/query_test.py
@@ -6,7 +6,7 @@
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# * Redistributions of source code must retain the above copyright
+#     * Redistributions of source code must retain the above copyright
 #       notice, this list of conditions and the following disclaimer.
 #     * Redistributions in binary form must reproduce the above copyright
 #       notice, this list of conditions and the following disclaimer in the
@@ -31,14 +31,15 @@
 __author__ = "Simon Oldfield"
 
 
-from datacube.api.model import DatasetType, Tile, Cell, Satellite
-from datacube.api.query import list_tiles, list_tiles_wkt, list_tiles_to_file, list_cells, list_cells_to_file
-from datacube.api.query import list_cells_missing, list_cells_missing_to_file
-from datacube.api.query import list_tiles_dtm
-from datacube.api.workflow import parse_date_min, parse_date_max
 import logging
 import os
 import csv
+from datacube.api.model import DatasetType, Tile, Cell, Satellite
+from datacube.api.query import list_tiles, list_tiles_wkt, list_tiles_to_file, list_cells, list_cells_to_file
+from datacube.api.query import MONTHS_BY_SEASON, Season
+from datacube.api.query import LS7_SLC_OFF_EXCLUSION, LS8_PRE_WRS_2_EXCLUSION
+from datacube.api.query import list_cells_missing, list_cells_missing_to_file, list_tiles_dtm
+from datacube.api.workflow import parse_date_min, parse_date_max
 from datetime import date
 
 
@@ -47,13 +48,24 @@ _log = logging.getLogger()
 
 def main():
 
-    # do_list_cells_by_xy_single()
+    do_list_cells_by_xy_single()
+
+    # do_list_cells_by_xy_single_no_ls7_slc_off()
+    # do_list_cells_by_xy_single_no_ls7_slc_off_no_ls8_pre_wrs_2()
+
+    do_list_cells_by_xy_single_months()
+
     # do_list_cells_by_xy_single_csv()
 
     # do_list_cells_missing_by_xy_single()
     # do_list_cells_missing_by_xy_single_csv()
 
-    do_list_tiles_by_xy_single()
+    # do_list_tiles_by_xy_single()
+
+    # do_list_tiles_by_xy_single_no_ls7_slc_off()
+    # do_list_tiles_by_xy_single_no_ls7_slc_off_no_ls8_wrs2()
+
+    # do_list_tiles_by_xy_single_months()
 
     # do_list_tiles_dtm_by_xy_single(config)
 
@@ -70,19 +82,70 @@ def main():
 
 
 def do_list_cells_by_xy_single(config=None):
-    cells = list_cells(x=[120], y=[-25], acq_min=parse_date_min("2014-01"), acq_max=parse_date_max("2014-01"),
-                       satellites=[Satellite.LS5],
-                       # dataset_types=[DatasetType.ARG25],
+    cells = list_cells(x=[120], y=[-25], acq_min=parse_date_min("2005"), acq_max=parse_date_max("2005"),
+                       satellites=[Satellite.LS5, Satellite.LS7, Satellite.LS8],
+                       dataset_types=[DatasetType.ARG25],
                        # dataset_types=[DatasetType.ARG25, DatasetType.PQ25],
                        # dataset_types=[DatasetType.ARG25, DatasetType.PQ25, DatasetType.FC25],
                        # dataset_types=[DatasetType.ARG25, DatasetType.PQ25, DatasetType.FC25, DatasetType.DSM],
                        # dataset_types=[DatasetType.ARG25, DatasetType.PQ25, DatasetType.FC25, DatasetType.DSM, DatasetType.DEM],
                        # dataset_types=[DatasetType.ARG25, DatasetType.PQ25, DatasetType.FC25, DatasetType.DSM, DatasetType.DEM, DatasetType.DEM_HYDROLOGICALLY_ENFORCED],
-                       dataset_types=[DatasetType.ARG25, DatasetType.PQ25, DatasetType.FC25, DatasetType.DSM, DatasetType.DEM, DatasetType.DEM_HYDROLOGICALLY_ENFORCED, DatasetType.DEM_SMOOTHED],
+                       # dataset_types=[DatasetType.ARG25, DatasetType.PQ25, DatasetType.FC25, DatasetType.DSM, DatasetType.DEM, DatasetType.DEM_HYDROLOGICALLY_ENFORCED, DatasetType.DEM_SMOOTHED],
                        config=config)
 
     for cell in cells:
-        _log.debug("Found cell xy = %s", cell.xy)
+        _log.info("Found cell xy = %s", cell.xy)
+
+
+def do_list_cells_by_xy_single_no_ls7_slc_off(config=None):
+    cells = list_cells(x=[120], y=[-25], acq_min=parse_date_min("2005"), acq_max=parse_date_max("2005"),
+                       satellites=[Satellite.LS5, Satellite.LS7, Satellite.LS8],
+                       dataset_types=[DatasetType.ARG25],
+                       # dataset_types=[DatasetType.ARG25, DatasetType.PQ25],
+                       # dataset_types=[DatasetType.ARG25, DatasetType.PQ25, DatasetType.FC25],
+                       # dataset_types=[DatasetType.ARG25, DatasetType.PQ25, DatasetType.FC25, DatasetType.DSM],
+                       # dataset_types=[DatasetType.ARG25, DatasetType.PQ25, DatasetType.FC25, DatasetType.DSM, DatasetType.DEM],
+                       # dataset_types=[DatasetType.ARG25, DatasetType.PQ25, DatasetType.FC25, DatasetType.DSM, DatasetType.DEM, DatasetType.DEM_HYDROLOGICALLY_ENFORCED],
+                       # dataset_types=[DatasetType.ARG25, DatasetType.PQ25, DatasetType.FC25, DatasetType.DSM, DatasetType.DEM, DatasetType.DEM_HYDROLOGICALLY_ENFORCED, DatasetType.DEM_SMOOTHED],
+                       exclude=[LS7_SLC_OFF_EXCLUSION],
+                       config=config)
+
+    for cell in cells:
+        _log.info("Found cell xy = %s", cell.xy)
+
+
+def do_list_cells_by_xy_single_no_ls7_slc_off_no_ls8_pre_wrs_2(config=None):
+    cells = list_cells(x=[120], y=[-25], acq_min=parse_date_min("2005"), acq_max=parse_date_max("2005"),
+                       satellites=[Satellite.LS5, Satellite.LS7, Satellite.LS8],
+                       dataset_types=[DatasetType.ARG25],
+                       # dataset_types=[DatasetType.ARG25, DatasetType.PQ25],
+                       # dataset_types=[DatasetType.ARG25, DatasetType.PQ25, DatasetType.FC25],
+                       # dataset_types=[DatasetType.ARG25, DatasetType.PQ25, DatasetType.FC25, DatasetType.DSM],
+                       # dataset_types=[DatasetType.ARG25, DatasetType.PQ25, DatasetType.FC25, DatasetType.DSM, DatasetType.DEM],
+                       # dataset_types=[DatasetType.ARG25, DatasetType.PQ25, DatasetType.FC25, DatasetType.DSM, DatasetType.DEM, DatasetType.DEM_HYDROLOGICALLY_ENFORCED],
+                       # dataset_types=[DatasetType.ARG25, DatasetType.PQ25, DatasetType.FC25, DatasetType.DSM, DatasetType.DEM, DatasetType.DEM_HYDROLOGICALLY_ENFORCED, DatasetType.DEM_SMOOTHED],
+                       exclude=[LS7_SLC_OFF_EXCLUSION, LS8_PRE_WRS_2_EXCLUSION],
+                       config=config)
+
+    for cell in cells:
+        _log.info("Found cell xy = %s", cell.xy)
+
+
+def do_list_cells_by_xy_single_months(config=None):
+    cells = list_cells(x=[120], y=[-25], acq_min=parse_date_min("2005"), acq_max=parse_date_max("2005"),
+                       satellites=[Satellite.LS5, Satellite.LS7, Satellite.LS8],
+                       dataset_types=[DatasetType.ARG25],
+                       # dataset_types=[DatasetType.ARG25, DatasetType.PQ25],
+                       # dataset_types=[DatasetType.ARG25, DatasetType.PQ25, DatasetType.FC25],
+                       # dataset_types=[DatasetType.ARG25, DatasetType.PQ25, DatasetType.FC25, DatasetType.DSM],
+                       # dataset_types=[DatasetType.ARG25, DatasetType.PQ25, DatasetType.FC25, DatasetType.DSM, DatasetType.DEM],
+                       # dataset_types=[DatasetType.ARG25, DatasetType.PQ25, DatasetType.FC25, DatasetType.DSM, DatasetType.DEM, DatasetType.DEM_HYDROLOGICALLY_ENFORCED],
+                       # dataset_types=[DatasetType.ARG25, DatasetType.PQ25, DatasetType.FC25, DatasetType.DSM, DatasetType.DEM, DatasetType.DEM_HYDROLOGICALLY_ENFORCED, DatasetType.DEM_SMOOTHED],
+                       months=MONTHS_BY_SEASON[Season.SUMMER],
+                       config=config)
+
+    for cell in cells:
+        _log.info("Found cell xy = %s", cell.xy)
 
 
 def do_list_cells_by_xy_single_csv(config=None):
@@ -161,13 +224,81 @@ def do_list_cells_missing_by_xy_single_csv(config=None):
 
 def do_list_tiles_by_xy_single():
     _log.info("Testing list_tiles...")
-    tiles = list_tiles(x=[120], y=[-20], acq_min=date(2002, 1, 1), acq_max=date(2002, 12, 31),
-                       satellites=[Satellite.LS7],
+    tiles = list_tiles(x=[120], y=[-20], acq_min=date(2005, 1, 1), acq_max=date(2005, 12, 31),
+                       satellites=[Satellite.LS5, Satellite.LS7, Satellite.LS8],
+                       dataset_types=[DatasetType.ARG25])
                        # dataset_types=[DatasetType.ARG25, DatasetType.PQ25, DatasetType.FC25])
-                       dataset_types=[DatasetType.ARG25, DatasetType.PQ25, DatasetType.FC25, DatasetType.DEM, DatasetType.NDVI])
+                       # dataset_types=[DatasetType.ARG25, DatasetType.PQ25, DatasetType.FC25, DatasetType.DEM, DatasetType.NDVI])
+
+    count = {Satellite.LS5: 0, Satellite.LS7: 0, Satellite.LS8: 0}
 
     for tile in tiles:
         _log.info("Found tile xy = %s acq date = [%s] NBAR = [%s]", tile.xy, tile.end_datetime, tile.datasets[DatasetType.ARG25].path)
+        count[tile.datasets[DatasetType.ARG25].satellite] += 1
+
+    _log.info(count)
+
+
+def do_list_tiles_by_xy_single_no_ls7_slc_off():
+    _log.info("Testing list_tiles NO LS7 SLC OFF...")
+
+    from datetime import date
+
+    tiles = list_tiles(x=[120], y=[-20], acq_min=date(2005, 1, 1), acq_max=date(2005, 12, 31),
+                       satellites=[Satellite.LS5, Satellite.LS7, Satellite.LS8],
+                       dataset_types=[DatasetType.ARG25],
+                       # dataset_types=[DatasetType.ARG25, DatasetType.PQ25, DatasetType.FC25],
+                       # dataset_types=[DatasetType.ARG25, DatasetType.PQ25, DatasetType.FC25, DatasetType.DEM, DatasetType.NDVI],
+                       exclude=[LS7_SLC_OFF_EXCLUSION])
+
+    count = {Satellite.LS5: 0, Satellite.LS7: 0, Satellite.LS8: 0}
+
+    for tile in tiles:
+        _log.debug("Found tile xy = %s acq date = [%s] NBAR = [%s]", tile.xy, tile.end_datetime, tile.datasets[DatasetType.ARG25].path)
+        count[tile.datasets[DatasetType.ARG25].satellite] += 1
+
+    _log.info(count)
+
+
+def do_list_tiles_by_xy_single_no_ls7_slc_off_no_ls8_wrs2():
+    _log.info("Testing list_tiles NO LS7 SLC OFF...")
+
+    from datetime import date
+
+    exclude = [LS7_SLC_OFF_EXCLUSION, LS8_PRE_WRS_2_EXCLUSION]
+
+    tiles = list_tiles(x=[120], y=[-20], acq_min=date(2005, 1, 1), acq_max=date(2005, 12, 31),
+                       satellites=[Satellite.LS5, Satellite.LS7, Satellite.LS8],
+                       dataset_types=[DatasetType.ARG25],
+                       # dataset_types=[DatasetType.ARG25, DatasetType.PQ25, DatasetType.FC25],
+                       # dataset_types=[DatasetType.ARG25, DatasetType.PQ25, DatasetType.FC25, DatasetType.DEM, DatasetType.NDVI],
+                       exclude=exclude)
+
+    count = {Satellite.LS5: 0, Satellite.LS7: 0, Satellite.LS8: 0}
+
+    for tile in tiles:
+        _log.debug("Found tile xy = %s acq date = [%s] NBAR = [%s]", tile.xy, tile.end_datetime, tile.datasets[DatasetType.ARG25].path)
+        count[tile.datasets[DatasetType.ARG25].satellite] += 1
+
+    _log.info(count)
+
+
+def do_list_tiles_by_xy_single_months():
+    _log.info("Testing list_tiles MONTHS...")
+    tiles = list_tiles(x=[120], y=[-20], acq_min=date(2005, 1, 1), acq_max=date(2005, 12, 31),
+                       satellites=[Satellite.LS5, Satellite.LS7, Satellite.LS8],
+                       dataset_types=[DatasetType.ARG25],
+                       # dataset_types=[DatasetType.ARG25, DatasetType.PQ25, DatasetType.FC25],
+                       # dataset_types=[DatasetType.ARG25, DatasetType.PQ25, DatasetType.FC25, DatasetType.DEM, DatasetType.NDVI],
+                       months=MONTHS_BY_SEASON[Season.SUMMER])
+
+    count = {Satellite.LS5: 0, Satellite.LS7: 0, Satellite.LS8: 0}
+
+    for tile in tiles:
+        _log.info("Found tile xy = %s acq date = [%s] NBAR = [%s]", tile.xy, tile.end_datetime, tile.datasets[DatasetType.ARG25].path)
+        count[tile.datasets[DatasetType.ARG25].satellite] += 1
+
+    _log.info(count)
 
 
 def do_list_tiles_by_xy_single_to_file(config):
